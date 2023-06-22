14165e5a7d620e81f7d5aab2decb170a1fd4363b
GisellyRock <gisellydsr@gmail.com>
2023-06-21 22:03:13 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if (request.url === '/produto') {
+      response.end(
+        JSON.stringify({
+          message: 'Rota de produto',
+        }),
+      );
+    }
+
+    if (request.url === '/usuario') {
+      response.end(
+        JSON.stringify({
+          message: 'Rota de usuário',
+        }),
+      );
+    }
+
+    response.end(
+      JSON.stringify({
+        message: 'qualquer outra rota',
+      }),
+    );
+  })
+  .listen(4001, () => console.log('servidor esta rodando na porta 4001'));
